b417016d93318a5d08f3a74f5109c4b4be9a9b39
Nikita Fedorov <nikitafedorov666@icloud.com>
2022-01-02 20:00:21 +0300

    go github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from 'react';
+import Counter from './components/Counter';
+import PostForm from './components/PostForm';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UL/button/MyButton';
+import MyInput from './components/UL/input/MyInput';
+import MySelect from './components/UL/select/MySelect';
+import './styles/app.css'
+
 
 function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'zzzz'},
+        {id: 2, title: 'python 2', body: 'bbbb'},
+        {id: 3, title: 'C++ 3', body: 'aaaa'}
+    ])
+
+    const [selectedSort, setSelectedSort] = useState('') 
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort)
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+    }
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <PostForm create={createPost} />
+        <hr style={{margin: '15px 0'}}/>
+        <div>
+            <MySelect
+            value={selectedSort}
+            onChange={sortPosts}
+                defaultValue="Сортировка"
+                options={[
+                    {value: 'title', name: 'По названию'},
+                    {value: 'body', name: 'По описанию'}
+                ]}
+            />
+        </div>
+        {posts.length !== 0
+           ? <PostList remove={removePost} posts={posts} title={'Посты про js'}/>
+           : <h1 style={{textAlign: 'center'}} >Посты не были найдены</h1> 
+        }
     </div>
   );
 }
